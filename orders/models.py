f99951d8f6cfbbfe50de9619c719ff7c9c2d1e88
Michelle-lele <fia_m@abv.bg>
2019-10-16 22:51:28 +0300

    Another unsuccessful try to sort the pizza model topping optiosn

--- orders/models.py
+++ orders/models.py
@@ -1,45 +1,59 @@
 from django.db import models
 
 # Create your models here.
-
 class abstractMenuItem(models.Model):
 	name = models.CharField(max_length=64)
-	price = models.DecimalField(decimal_places=2,max_digits=10, blank=True, null=True)
+	price = models.DecimalField(decimal_places=2,max_digits=10, default=0)
+
+	class Meta:
+		abstract = True
+
+class abstractToppingConfig(models.Model):
+	toppingOption = models.CharField(max_length=1, choices=(('0','No toppings'), ('1','Pre-selected'), ('2','No more than..'), ('3','Select from all')), default = ('0','No toppings'),help_text="Allow users select toppings")
+
+	def configureToppings(toppingOption):
+		if toppingOption == '0':
+			pass
+		if toppingOption == '2':
+			maxToppings =  models.PositiveSmallIntegerField(blank=True, null=True)
+			toppings = models.CharField
+			return maxToppings
 
 	class Meta:
 		abstract = True
 
 
-ITEM_SIZES = (
-			('S','Small'),
-			('L','Large'),
-			)
-
 class Size(models.Model):
-	def size_default():
-		return ('S','Small')
-
-	size = models.CharField(choices=ITEM_SIZES, max_length=1, blank=False, default=size_default())
+	size = models.CharField(max_length=64)
 
 	def __str__(self):
 		return f"{self.size}"
 
+class pizzaType(models.Model):
+	type = models.CharField(max_length=64)
+
+	def __str__(self):
+		return f"{self.type}"
+
 class Topping(abstractMenuItem):
+
 	def __str__(self):
 		return f"{self.name}"
-
+ 
 class Pizza(abstractMenuItem):
-	type = models.CharField(max_length=64)
-	toppings = models.ManyToManyField(Topping, blank=True)
-	maxToppings =  models.IntegerField(blank=True)
+	type = models.ForeignKey(pizzaType, on_delete=models.CASCADE)
 	size = models.ForeignKey(Size, on_delete=models.CASCADE)
+	
+	toppingConfiguration = abstractToppingConfig()
+	maxToppings = toppingConfiguration.configureToppings(toppingOption)
 
 	def __str__(self):
-		return f"{self.type} {self.name}, {self.maxToppings} toppings, {self.size.name}"
+		return f"{self.type} {self.name}, {self.maxToppings} toppings, {self.size}"
 
 class Sub(abstractMenuItem):
-	toppings = models.ManyToManyField(Topping, blank=True)
 	size = models.ForeignKey(Size, on_delete=models.CASCADE)
+
+	additions = []
 
 	def __str__(self):
 		return f"{self.name} {self.size}"	
