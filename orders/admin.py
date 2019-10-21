e37cb372615f9e73241ffd830ca1221c5645759f
Michelle-lele <fia_m@abv.bg>
2019-10-21 22:43:49 +0300

    Coming closer to configuring topping options WIP

--- orders/admin.py
+++ orders/admin.py
@@ -2,11 +2,47 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Topping)
-admin.site.register(Pizza)
-admin.site.register(Sub)
-admin.site.register(Pasta)
-admin.site.register(Salad)
-admin.site.register(dinnerPlatter)
-admin.site.register(Size)
-admin.site.register(pizzaType)
+admin.site.site_header = 'Pinnochio\'s Pizza Orders'
+
+class ToppingAdmin(admin.ModelAdmin):
+	list_display = ('name', 'price')
+
+admin.site.register(Topping, ToppingAdmin)
+
+class PizzaAdmin(admin.ModelAdmin):
+	list_display = ('type','name','size','toppingOption','price')
+	list_filter = ('type', 'size', 'toppingOption')
+	fieldsets = (
+        (None, {
+            'fields': ('name',('type', 'size'), 'price')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': (('toppingOption', 'maxToppings'), 'toppings'),
+        }),
+    )
+
+admin.site.register(Pizza, PizzaAdmin)
+
+class SubAdmin(admin.ModelAdmin):
+	list_display = ('name', 'size', 'price')
+	list_filter = ('size',)
+	
+
+admin.site.register(Sub, SubAdmin)
+
+class PastaAdmin(admin.ModelAdmin):
+	list_display=('name','price')
+
+admin.site.register(Pasta, PastaAdmin)
+
+class SaladAdmin(admin.ModelAdmin):
+	list_display = ('name', 'price')
+
+admin.site.register(Salad, SaladAdmin)
+
+class dinnerPlatterAdmin(admin.ModelAdmin):
+	list_display= ('name','size', 'price')
+	list_filter = ('size',)
+
+admin.site.register(dinnerPlatter, dinnerPlatterAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 # Create your models here.
 class abstractMenuItem(models.Model):
@@ -10,14 +11,31 @@ class abstractMenuItem(models.Model):
 
 class abstractToppingConfig(models.Model):
 	toppingOption = models.CharField(max_length=1, choices=(('0','No toppings'), ('1','Pre-selected'), ('2','No more than..'), ('3','Select from all')), default = ('0','No toppings'),help_text="Allow users select toppings")
+	maxToppings =  models.PositiveSmallIntegerField(blank=True, null=True, default=0)
+	toppings = models.ManyToManyField("Topping", blank=True)
+
+	'''
+	models.CharField(validators=[validate_comma_separated_integer_list],max_length=200, blank=True, null=True,default='')
+	'''
 
 	def configureToppings(toppingOption):
 		if toppingOption == '0':
-			pass
-		if toppingOption == '2':
-			maxToppings =  models.PositiveSmallIntegerField(blank=True, null=True)
-			toppings = models.CharField
-			return maxToppings
+			#set maxtoppings & toppings to disabled? how?
+			return 1
+		elif toppingOption == '1':
+			#allow admin to select from all available toppings to show only those to user. 
+			#how? maxtoppings to disabled
+			#toppings enabled only
+			return 1
+		elif toppingOption == '2':
+			#admin doesn't have to preselect the toppings
+			#maxtoppings to enabled only
+			#toppings disabled
+			return 1
+		elif toppingOption == '3':
+			#maxtoppings to be set to the number of toppings- disabled
+			#toppings disabled
+			return 1
 
 	class Meta:
 		abstract = True
@@ -36,19 +54,15 @@ class pizzaType(models.Model):
 		return f"{self.type}"
 
 class Topping(abstractMenuItem):
-
 	def __str__(self):
 		return f"{self.name}"
  
-class Pizza(abstractMenuItem):
+class Pizza(abstractMenuItem, abstractToppingConfig):
 	type = models.ForeignKey(pizzaType, on_delete=models.CASCADE)
 	size = models.ForeignKey(Size, on_delete=models.CASCADE)
-	
-	toppingConfiguration = abstractToppingConfig()
-	maxToppings = toppingConfiguration.configureToppings(toppingOption)
 
 	def __str__(self):
-		return f"{self.type} {self.name}, {self.maxToppings} toppings, {self.size}"
+		return f"{self.type} {self.name}, {self.size}"
 
 class Sub(abstractMenuItem):
 	size = models.ForeignKey(Size, on_delete=models.CASCADE)
